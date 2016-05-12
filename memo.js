4fedc7f8aff912cc58efb4cd0053527d79c34dfb
Rickard Eneroth <rickard.eneroth@beamonpeople.se>
2016-05-12 08:17:30 +0200

    Fixes

--- memo.js
+++ memo.js
@@ -3,41 +3,30 @@
        $scope.myForm = {};
        $scope.myForm.name = "Rickard";
        $scope.myForm.memo = "";
+       $scope.myForm.id = "";
 
-     $scope.myForm.submitTheFormGetAll = function(item, event) {
+     $scope.myForm.submitTheFormGetAll = function() {
        
-       	var dataObject = { name : $scope.myForm.name, memo : $scope.myForm.memo };
-       	
 	$http.get('https://immense-scrubland-69667.herokuapp.com/memo/getall').success(function(data) {$scope.memo_response = data;});
      }
 
-     $scope.myForm.submitTheFormGet = function(item, event) {
-       
-       	var dataObject = { name : $scope.myForm.name, memo : $scope.myForm.memo };
-       	
+     $scope.myForm.submitTheFormGet = function() {
+        	
 	$http.get('https://immense-scrubland-69667.herokuapp.com/memo/getall?userId=' + $scope.myForm.name).success(function(data) {$scope.memo_response = data;});
      }
      
-     $scope.myForm.submitTheFormDel = function(item, event) {
-            
-        var dataObject = { name : $scope.myForm.name, memo : $scope.myForm.memo };
-     	
-     	$http.get('https://immense-scrubland-69667.herokuapp.com/memo/deleteall?userId=' + $scope.myForm.name).success(function(data) {});
-     	
-     	$http.get('https://immense-scrubland-69667.herokuapp.com/memo/getall').success(function(data) {$scope.memo_response = data;});
-     	
-     	alert("Alla memos för " + $scope.myForm.name + " borttagna!");
+     $scope.myForm.submitTheFormDelete = function(id) {
+        
+	$http.delete('https://immense-scrubland-69667.herokuapp.com/memo/delete?id=' + id).success(function(data) {
+		$http.get('https://immense-scrubland-69667.herokuapp.com/memo/getall').success(function(data) {$scope.memo_response = data;});
+	});
      }
      
-     $scope.myForm.submitTheFormAdd = function(item, event) {
-                 
-     	var dataObject = { name : $scope.myForm.name, memo : $scope.myForm.memo };
-          	
-     	$http.get('https://immense-scrubland-69667.herokuapp.com/memo/add?userId=' + $scope.myForm.name + '&memo=' + $scope.myForm.memo).success(function(data) {});
+     $scope.myForm.submitTheFormAdd = function() {
+                   	
+     	$http.post('https://immense-scrubland-69667.herokuapp.com/memo/add?userId=' + $scope.myForm.name + '&memo=' + $scope.myForm.memo).success(function(data) {});
           	
      	$http.get('https://immense-scrubland-69667.herokuapp.com/memo/getall').success(function(data) {$scope.memo_response = data;});
-          	
-     	alert("Nytt memo för " + $scope.myForm.name + " tillagt!");
      }
      
   });

101a3adff4ccfe504562e60a27c145b8c272d99a
Rickard <rickard_eneroth@hotmail.com>
2016-05-15 19:31:35 +0200

    baseUrl

--- memo.js
+++ memo.js
@@ -4,29 +4,27 @@
        $scope.myForm.name = "Rickard";
        $scope.myForm.memo = "";
        $scope.myForm.id = "";
+       baseUrl = "https://immense-scrubland-69667.herokuapp.com";
 
      $scope.myForm.submitTheFormGetAll = function() {
-       
-	$http.get('https://immense-scrubland-69667.herokuapp.com/memo/getall').success(function(data) {$scope.memo_response = data;});
+	$http.get(baseUrl + '/memo/getall').success(function(data) {$scope.memo_response = data;});
      }
 
      $scope.myForm.submitTheFormGet = function() {
-        	
-	$http.get('https://immense-scrubland-69667.herokuapp.com/memo/getall?userId=' + $scope.myForm.name).success(function(data) {$scope.memo_response = data;});
+	$http.get(baseUrl + '/memo/getall?userId=' + $scope.myForm.name).success(function(data) {$scope.memo_response = data;});
      }
      
      $scope.myForm.submitTheFormDelete = function(id) {
         
-	$http.delete('https://immense-scrubland-69667.herokuapp.com/memo/delete?id=' + id).success(function(data) {
-		$http.get('https://immense-scrubland-69667.herokuapp.com/memo/getall').success(function(data) {$scope.memo_response = data;});
+	$http.delete(baseUrl + '/memo/delete?id=' + id).success(function(data) {
+		$http.get(baseUrl + '/memo/getall').success(function(data) {$scope.memo_response = data;});
 	});
      }
      
-     $scope.myForm.submitTheFormAdd = function() {
-                   	
-     	$http.post('https://immense-scrubland-69667.herokuapp.com/memo/add?userId=' + $scope.myForm.name + '&memo=' + $scope.myForm.memo).success(function(data) {});
-          	
-     	$http.get('https://immense-scrubland-69667.herokuapp.com/memo/getall').success(function(data) {$scope.memo_response = data;});
+     $scope.myForm.submitTheFormAdd = function() {   	
+     	$http.post(baseUrl + '/memo/add?userId=' + $scope.myForm.name + '&memo=' + $scope.myForm.memo).success(function(data) {});
+     	$http.get(baseUrl + '/memo/getall').success(function(data) {$scope.memo_response = data;});
      }
      
   });
+  
